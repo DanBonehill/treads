fff66a6b63b10af226ae05de8faa4347ce6a5dcc
DanBonehill <DanBonehill@me.com>
2018-03-29 16:37:43 +0100

    Add BeginRunVC

---
+++ treads/Controller/BeginRunVC.swift
@@ -0,0 +1,16 @@
+//
+//  BeginRunVC.swift
+//  treads
+//
+//  Created by Daniel Bonehill on 29/03/2018.
+//  Copyright © 2018 Daniel Bonehill. All rights reserved.
+//
+
+import UIKit
+
+class BeginRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
