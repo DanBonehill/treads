0aa8a100d26c313cc41e124a09a672d294f16b00
DanBonehill <DanBonehill@me.com>
2018-03-29 22:01:31 +0100

    Create CurrentRunVC & UI

---
+++ treads/Controller/CurrentRunVC.swift
@@ -0,0 +1,16 @@
+//
+//  CurrentRunVC.swift
+//  treads
+//
+//  Created by Daniel Bonehill on 29/03/2018.
+//  Copyright © 2018 Daniel Bonehill. All rights reserved.
+//
+
+import UIKit
+
+class CurrentRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
