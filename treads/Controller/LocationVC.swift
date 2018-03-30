119338c26d685fad047d7c1ce29c222bec65d1c7
DanBonehill <DanBonehill@me.com>
2018-03-30 13:51:29 +0100

    Map View setup

---
+++ treads/Controller/LocationVC.swift
@@ -0,0 +1,28 @@
+//
+//  LocationVC.swift
+//  treads
+//
+//  Created by Daniel Bonehill on 30/03/2018.
+//  Copyright © 2018 Daniel Bonehill. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationVC: UIViewController, MKMapViewDelegate {
+    
+    var manager: CLLocationManager?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        manager = CLLocationManager()
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.activityType = .fitness
+    }
+    
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            manager?.requestWhenInUseAuthorization()
+        }
+    }
+}
